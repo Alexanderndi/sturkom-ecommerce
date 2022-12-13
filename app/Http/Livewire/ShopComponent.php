c8d0e76f0772499dbfb1b859518cab907194b9de
Ndifreke Alexander <ndifrekealexander@Ndifrekes-MacBook-Pro.local>
2022-12-13 14:50:53 +0100

    Resolved OrderBy errors

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -12,6 +12,7 @@ class ShopComponent extends Component
     use WithPagination;
 
     public $pageSize = 12;
+    public $orderBy = "Default Sorting";
     public function store($product_id,$product_name,$product_price)
     {
         Cart::add($product_id, $product_name, 1, $product_price)->associate('\App\Models\Product');
@@ -22,9 +23,26 @@ class ShopComponent extends Component
     {
         $this->pageSize = $size;
     }
+
+    public function changeOrderBy($order)
+    {
+        $this->orderBy = $order;
+    }
     public function render()
     {
+        if($this->orderBy == 'Price: Low to High')
+        {
+            $products = Product::orderBy('regular_price', 'ASC')->paginate($this->pageSize);
+        }
+        else if($this->orderBy == 'Price: High to Low'){
+            $products = Product::orderBy('regular_price', 'DESC')->paginate($this->pageSize);
+        }
+        else if($this->orderBy == 'Sort By Newness'){
+            $products = Product::orderBy('created_at', 'DESC')->paginate($this->pageSize);
+        }
+        else{
         $products = Product::paginate($this->pageSize);
+        }
         return view('livewire.shop-component',['products'=>$products]);
     }
 }
