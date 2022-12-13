09ea390bd252e5f28388b8b709239f69ee5424e6
Ndifreke Alexander <ndifrekealexander@Ndifrekes-MacBook-Pro.local>
2022-12-13 14:33:26 +0100

    Resolving product sorting and products per page

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -10,15 +10,21 @@ use Cart;
 class ShopComponent extends Component
 {
     use WithPagination;
+
+    public $pageSize = 12;
     public function store($product_id,$product_name,$product_price)
     {
         Cart::add($product_id, $product_name, 1, $product_price)->associate('\App\Models\Product');
         session()->flash('success_message', "Item added into Cart");
         return redirect()->route('shop.cart');
     }
+    public function changePageSize($size)
+    {
+        $this->pageSize = $size;
+    }
     public function render()
     {
-        $products = Product::paginate(12);
+        $products = Product::paginate($this->pageSize);
         return view('livewire.shop-component',['products'=>$products]);
     }
 }
