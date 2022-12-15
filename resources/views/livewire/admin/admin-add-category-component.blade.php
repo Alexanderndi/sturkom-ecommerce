c3bba4f45072206713bdd11d95dd30060510245c
Ndifreke Alexander <ndifrekealexander@Ndifrekes-MacBook-Pro.local>
2022-12-15 11:20:50 +0100

    Add Category Component

--- resources/views/livewire/admin/admin-add-category-component.blade.php
+++ resources/views/livewire/admin/admin-add-category-component.blade.php
@@ -32,14 +32,23 @@
                             </div>
                         </div>
                         <div class="card-body">
-                            <form action="">
+                            @if (Session::has('message'))
+                                <div class="alert alert-success" role="alert">{{ Session::get('message') }}</div>
+                            @endif
+                            <form wire:submit.prevent="storeCategory">
                                 <div class="md-3 mt-3">
                                     <label for="name" class="form-label">Name</label>
-                                    <input type="text" name="name" class="form-control" placeholder="Enter category name" />
+                                    <input type="text" name="name" class="form-control" placeholder="Enter category name" wire:model="name" wire:keyup="generateSlug" />
+                                    @error('name')
+                                        <p class="text-danger">{{$message}}</p>
+                                    @enderror
                                 </div>
                                 <div class="md-3 mt-3">
                                     <label for="slug" class="form-label">Slug</label>
-                                    <input type="text" name="slug" class="form-control" placeholder="Enter category slug" />
+                                    <input type="text" name="slug" class="form-control" placeholder="Enter category slug" wire:model="slug"/>
+                                    @error('slug')
+                                        <p class="text-danger">{{$message}}</p>
+                                    @enderror
                                 </div>
                                 <button type="submit" class="btn btn-primary float-end">Submit</button>
                             </form>
